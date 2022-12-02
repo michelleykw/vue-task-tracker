6e58e229a39849b6db2fb1e3b66de1cf0885d792
michelleykw <themichelleyong@gmail.com>
2022-12-02 16:35:08 +0800

    Init first vue project

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log('click');
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">
+        {{ text }}
+    </button>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green" />
+    </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
