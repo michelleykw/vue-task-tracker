d844d3a0c4b4a6619b5f2bb52100300db29a87e0
michelleykw <themichelleyong@gmail.com>
2022-12-02 17:27:58 +0800

    Added delete and toggled task

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
 <script>
 import Header from './components/Header.vue';
+import Tasks from './components/Tasks.vue';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Learn Vue',
+        day: 'December 2nd at 7pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Learn iOS Dev',
+        day: 'December 12nd at 12pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Learn Flutter',
+        day: 'December 22nd at 1pm',
+        reminder: false,
+      }
+    ]
   }
 }
 </script>
 
 <template>
-  <header>
-    <div class="container">
-      <Header title="Task Tracker" />
-    </div>
-  </header>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  </div>
 </template>
 
 <style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<script>
+import TaskItem from './TaskItem.vue';
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        TaskItem
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
+
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <TaskItem
+            @toggle-reminder="$emit('toggle-reminder', task.id)"
+            @delete-task="$emit('delete-task', task.id)" 
+            :task="task" 
+        />
+    </div>
+</template>
